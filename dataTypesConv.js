8b28cdd4a48b900c6266e828e604f37e193eaed2
zammar-ahmar <159016048+zammar-ahmar@users.noreply.github.com>
2025-09-28 12:59:20 +0500

    added data

--- dataTypesConv.js
+++ dataTypesConv.js
@@ -1,11 +1,15 @@
-let sumNumber = 56
-
-
-// let changeToString = 
-
-
-
-
-
+let sumNumber = null
 
 console.log( typeof sumNumber)
+
+ let valueInNumber = Number(sumNumber)
+ console.log(valueInNumber)
+ console.log( typeof valueInNumber)
+
+// "56" => 56
+//  "56abc" => NaN
+//  true => 1 / false => 0
+// undefined => NaN
+// null => 0
+// boolean [""=> false / "added value " => true] 
+
