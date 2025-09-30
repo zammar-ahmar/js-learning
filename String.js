c5aacd9df8e239fc41ba4887a3c83d542eddaf4a
zammar-ahmar <159016048+zammar-ahmar@users.noreply.github.com>
2025-09-30 17:08:10 +0500

    string js file added

---
+++ String.js
@@ -0,0 +1,7 @@
+const name = new String("ali")
+const age = new String("20")
+
+console.log(`hello my Name is ${name.length} and  im ${age+1} years old`)
+
+const cuzzy =  [ "saad","ali","zammar","john"]
+ console.log(`my friends name are ${cuzzy.ToUpperCase}`)
