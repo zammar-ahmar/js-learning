6131894d3f6f8afe722efea2b1b61e2fbf984877
zammar-ahmar <159016048+zammar-ahmar@users.noreply.github.com>
2025-10-04 12:54:39 +0500

    practiced strings

--- String.js
+++ String.js
@@ -1,7 +1,19 @@
 const name = new String("ali")
 const age = new String("20")
 
-console.log(`hello my Name is ${name.length} and  im ${age+1} years old`)
+console.log(`hello my Name is ${name.toUpperCase()} and  im ${age+1} years old`)
 
-const cuzzy =  [ "saad","ali","zammar","john"]
- console.log(`my friends name are ${cuzzy.ToUpperCase}`)
+// const cuzzy = ["saad", "ali", "zammar", "john"];
+// document.getElementById('frnds').innerHTML = cuzzy.map(name => name.toUpperCase()).join(' ');
+
+
+
+
+let sss = "  zammar  "
+console.log(sss);
+console.log(sss.trim())
+
+ const url = "www.zammar20%domain.com"
+  console.log(url.replace("20%" ,"__"))
+  console.log(url.includes("zammar"))
+  console.log(url.includes("ali"))
