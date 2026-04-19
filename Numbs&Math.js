5df73ef27f7b7aa9b97dfe6054cef57f83229073
zammar-ahmar <159016048+zammar-ahmar@users.noreply.github.com>
2026-04-19 14:37:31 +0500

    maths n numb

---
+++ Numbs&Math.js
@@ -0,0 +1,22 @@
+const number = 1000
+console.log(number);
+  const loan = new Number(1000);
+  console.log(loan );
+  console.log(loan.toFixed(2 ));
+     
+  const num2 = new Number(333.867);
+  console.log(num2.toPrecision(3));
+  
+
+    const money = 2000000
+    console.log(money.toLocaleString());
+    
+    console.log(Math);
+    console.log(Math.abs(-5));
+    console.log(Math.round(4.6));
+    console.log(Math.ceil(4.1));
+    console.log(Math.floor(4.1));
+    console.log(Math.random());
+    // THE VALUE REMAINS BTW 0 N 1 IN Math.random
+    console.log((Math.random()*10)+ 1);
+    
