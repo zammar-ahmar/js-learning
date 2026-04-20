0f650090263fb605cb39411b0a61ff085ea20958
zammar-ahmar <159016048+zammar-ahmar@users.noreply.github.com>
2026-04-20 11:18:02 +0500

    new file

---
+++ dateNtime.js
@@ -0,0 +1,9 @@
+// Dates
+let myDate = new Date();
+console.log(myDate);
+
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
