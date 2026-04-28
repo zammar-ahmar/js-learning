07fa756d9186fb3ac637213261181e8ea6817ce0
zammar-ahmar <159016048+zammar-ahmar@users.noreply.github.com>
2026-04-28 12:41:16 +0500

    switchJS

---
+++ switch.js
@@ -0,0 +1,33 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+const months = 3
+
+
+switch (months) {
+    case 1:
+        console.log("jan");   
+        break;
+    case 2:
+        console.log("feb");   
+        break;
+    case 3:
+        console.log("mar");   
+        break;
+    case 4:
+        console.log("apr");   
+        break;
+
+    default:
+
+    console.log("default case");
+    
+        break;
+}
