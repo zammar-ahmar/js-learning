15712b00ee63a482f5624ecc57415f60c6aa8271
zammar-ahmar <159016048+zammar-ahmar@users.noreply.github.com>
2026-04-28 12:32:11 +0500

    added if-else cond

---
+++ one.js
@@ -0,0 +1,58 @@
+// if (condition) {
+    
+// }
+
+// const isUserLoggedIn= true
+
+// if (2==2) {
+//     console.log("3 is bigger")
+// }
+   
+// const temp = 33
+// if (temp != 35) {
+//     console.log("temp is high")
+// }else{
+//     console.log("temp is low")
+// }
+
+
+// < ,> >= ,<= ==, === , !== ,!=
+
+const cash = 1000
+ if (cash<500) {
+    console.log("cash is less than 500")
+    
+ }else if (cash<750) {
+        console.log("cash is less than 750")
+
+    
+ }else{
+        console.log("cash is less than 1050")
+
+ }
+
+
+
+
+ const zammar= true
+ const ali = true
+  
+ if (zammar && ali && 2==2) {
+
+
+    console.log("all true")
+    
+ }else {
+    console.log("not all true")
+
+ }
+
+
+
+ const user1=true
+ const user2=false
+
+ if (user1 || user2) {
+    console.log("if any one condition is true code runs")
+    
+ }
